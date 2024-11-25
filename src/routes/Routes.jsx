8fb6b5ed679ebdf80aa2da696adfe1276ff129da
vanesaromeo <vaneroeo09@gmail.com>
2024-11-25 20:36:48 -0300

    hice cambios

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,14 +1,15 @@
 import { Routes as ReactDomRoutes, Route } from 'react-router-dom';
 import { Conocenos } from '../pages/Conocenos/Conocenos';
 import { Productos } from '../pages/Productos/Productos';
-import Home from '../pages/home/Home';
+
 import Formulario from '../pages/formulario/formulario';
 import Checkout from './../pages/checkOut/CheckOut';
+import Home from './../pages/Home/Home';
 
 function Routes() {
   return (
     <ReactDomRoutes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home/>} />
       <Route path="/Conocenos" element={<Conocenos />} />
       <Route path="/productos" element={<Productos />} />
       <Route path="/formulario" element={<Formulario />} />
